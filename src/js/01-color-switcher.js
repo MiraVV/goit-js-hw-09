e14aab8fcb12a7516ae6f0a8d581b8ea532b1923
ЮРІЙ <v16m06a@gmail.com>
2022-06-05 01:55:32 +0300

    color-swicher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs ={
+    body: document.querySelector("body"),
+    buttonStart : document.querySelector("[data-start]"),
+    buttonStop : document.querySelector("[data-stop]"),
+}
 
+const CHANGECOLOR_DELAY = 1000;
+let setColor = null;
+
+refs.buttonStart.addEventListener("click", changeColor);
+refs.buttonStop.addEventListener("click", stopChangeColor);
+refs.buttonStop.setAttribute("disabled", true);
+
+function changeColor() {
+    setColor = setInterval(() => {
+        const newColor = getRandomHexColor();
+        refs.body.style.backgroundColor = newColor;
+        console.log(newColor)}
+   , CHANGECOLOR_DELAY);
+   refs.buttonStart.setAttribute("disabled", true);
+   refs.buttonStop.removeAttribute("disabled");
+}
+
+function stopChangeColor(){
+    clearInterval(setColor);
+    refs.buttonStart.removeAttribute("disabled");
+    refs.buttonStop.setAttribute("disabled", true);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
